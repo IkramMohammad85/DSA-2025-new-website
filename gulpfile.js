0bdd991eb666a3801df0bc4206308591c3008f81
IkramMohammad85 <ikramsonu@gmail.com>
2025-08-06 16:10:58 +0530

    updated the gulp file and for mapping URL for only man.css not min.css

--- gulpfile.js
+++ gulpfile.js
@@ -7,60 +7,84 @@ const babel = require("gulp-babel");
 const rename = require('gulp-rename');
 const sourcemaps = require('gulp-sourcemaps');
 const fileInclude = require("gulp-file-include");
+const replace = require('gulp-replace');
 const browserSync = require('browser-sync').create();
 
-// Compile SCSS → Minified CSS
-function styles() {
-  return gulp.src(['./src/scss/main.scss', './src/scss/mockups/*.scss'])
+// File paths
+const paths = {
+  scss: ['./src/scss/main.scss', './src/scss/mockups/*.scss'],
+  js: [
+    './src/js/utilities/helpers.js',
+    './src/js/components/*.js',
+    './src/js/main.js'
+  ],
+  html: 'src/html/**/*.html',
+  cssDest: './dist/css',
+  jsDest: './dist/js',
+  htmlDest: './dist'
+};
+
+// SCSS → Unminified CSS (with source map & comment)
+function stylesDev() {
+  return gulp.src(paths.scss)
+    .pipe(sourcemaps.init())
+    .pipe(sass().on('error', sass.logError))
+    .pipe(sourcemaps.write('.')) // Keeps sourceMappingURL comment
+    .pipe(gulp.dest(paths.cssDest))
+    .pipe(browserSync.stream());
+}
+
+// SCSS → Minified CSS (without sourceMappingURL comment)
+function stylesProd() {
+  return gulp.src(paths.scss)
     .pipe(sourcemaps.init())
     .pipe(sass().on('error', sass.logError))
     .pipe(cleanCSS())
     .pipe(rename({ suffix: '.min' }))
     .pipe(sourcemaps.write('.'))
-    .pipe(gulp.dest('./dist/css'))
+    .pipe(replace(/\/\*#\s*sourceMappingURL=.*\*\//g, '')) // Removes source map comment
+    .pipe(gulp.dest(paths.cssDest))
     .pipe(browserSync.stream());
 }
 
-// Minify & Bundle JS from components and utilities
+// JS: Minify & Bundle
 function scripts() {
-  return gulp.src([
-      './src/js/utilities/helpers.js',
-      './src/js/components/*.js',
-      './src/js/main.js'
-    ])
+  return gulp.src(paths.js)
     .pipe(sourcemaps.init())
     .pipe(concat('main.js'))
     .pipe(babel({ presets: ["@babel/env"] }))
-    .pipe(terser()) // Corrected from uglify()
+    .pipe(terser())
     .pipe(rename({ suffix: '.min' }))
     .pipe(sourcemaps.write('.'))
-    .pipe(gulp.dest('./dist/js'))
+    .pipe(gulp.dest(paths.jsDest))
     .pipe(browserSync.stream());
 }
 
-// Process HTML includes
+// HTML includes
 function html() {
-  return gulp.src("src/html/**/*.html")
+  return gulp.src(paths.html)
     .pipe(fileInclude({ prefix: "@@", basepath: "@file" }))
-    .pipe(gulp.dest("dist"))
+    .pipe(gulp.dest(paths.htmlDest))
     .pipe(browserSync.stream());
 }
 
-// Watch Files and Start Local Server
+// Watch files + Live reload
 function watchFiles() {
   browserSync.init({
     server: { baseDir: "dist", index: "index.html" },
     port: 8080,
   });
 
-  gulp.watch("src/scss/**/*.scss", styles);
+  gulp.watch("src/scss/**/*.scss", gulp.series(stylesDev, stylesProd));
   gulp.watch("src/js/**/*.js", scripts);
   gulp.watch("src/html/**/*.html", html);
 }
-// Build Task (Run all tasks sequentially)
-gulp.task('build', gulp.series(styles, scripts, html));
-// Define Tasks
-gulp.task("html", html);
+
+// Gulp tasks
+gulp.task("stylesDev", stylesDev);
+gulp.task("stylesProd", stylesProd);
 gulp.task("scripts", scripts);
-gulp.task("watch", gulp.series("html", "scripts", watchFiles));
-gulp.task("default", gulp.series(gulp.parallel(styles, scripts, html), watchFiles));
+gulp.task("html", html);
+gulp.task("watch", gulp.series("html", "scripts", stylesDev, stylesProd, watchFiles));
+gulp.task("build", gulp.series(stylesDev, stylesProd, scripts, html));
+gulp.task("default", gulp.series("build", watchFiles));

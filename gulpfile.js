3dea49e6d25bd3f1816d34eeafda04aa69e70f34
IkramMohammad85 <ikramsonu@gmail.com>
2026-01-08 13:08:14 +0530

    updated the cache issue

--- gulpfile.js
+++ gulpfile.js
@@ -1,90 +1,96 @@
-const gulp = require('gulp');
-const sass = require('gulp-sass')(require('sass'));
-const cleanCSS = require('gulp-clean-css');
-const terser = require('gulp-terser');
-const concat = require('gulp-concat');
+const { src, dest, watch, series, parallel } = require("gulp");
+const sass = require("gulp-sass")(require("sass"));
+const cleanCSS = require("gulp-clean-css");
+const terser = require("gulp-terser");
+const concat = require("gulp-concat");
 const babel = require("gulp-babel");
-const rename = require('gulp-rename');
-const sourcemaps = require('gulp-sourcemaps');
+const rename = require("gulp-rename");
+const sourcemaps = require("gulp-sourcemaps");
 const fileInclude = require("gulp-file-include");
-const replace = require('gulp-replace');
-const browserSync = require('browser-sync').create();
+const replace = require("gulp-replace");
+const browserSync = require("browser-sync").create();
+
+const isNetlify = process.env.NETLIFY === "true";
 
 // File paths
 const paths = {
-  scss: ['./src/scss/main.scss', './src/scss/mockups/*.scss'],
+  scss: ["./src/scss/main.scss", "./src/scss/mockups/*.scss"],
   js: [
-    './src/js/utilities/helpers.js',
-    './src/js/components/*.js',
-    './src/js/main.js'
+    "./src/js/utilities/helpers.js",
+    "./src/js/components/*.js",
+    "./src/js/main.js",
   ],
-  html: 'src/html/**/*.html',
-  cssDest: './dist/css',
-  jsDest: './dist/js',
-  htmlDest: './dist'
+  html: "src/html/**/*.html",
+  cssDest: "./dist/css",
+  jsDest: "./dist/js",
+  htmlDest: "./dist",
 };
 
-// SCSS → Unminified CSS (with source map & comment)
+// -------------------- STYLES --------------------
+
+// DEV CSS (with sourcemaps)
 function stylesDev() {
-  return gulp.src(paths.scss)
+  return src(paths.scss)
     .pipe(sourcemaps.init())
-    .pipe(sass().on('error', sass.logError))
-    .pipe(sourcemaps.write('.')) // Keeps sourceMappingURL comment
-    .pipe(gulp.dest(paths.cssDest))
-    .pipe(browserSync.stream());
+    .pipe(sass().on("error", sass.logError))
+    .pipe(sourcemaps.write("."))
+    .pipe(dest(paths.cssDest))
+    .pipe(isNetlify ? dest(paths.cssDest) : browserSync.stream());
 }
 
-// SCSS → Minified CSS (without sourceMappingURL comment)
+// PROD CSS (minified, no sourcemap comment)
 function stylesProd() {
-  return gulp.src(paths.scss)
-    .pipe(sourcemaps.init())
-    .pipe(sass().on('error', sass.logError))
+  return src(paths.scss)
+    .pipe(sass().on("error", sass.logError))
     .pipe(cleanCSS())
-    .pipe(rename({ suffix: '.min' }))
-    .pipe(sourcemaps.write('.'))
-    .pipe(replace(/\/\*#\s*sourceMappingURL=.*\*\//g, '')) // Removes source map comment
-    .pipe(gulp.dest(paths.cssDest))
-    .pipe(browserSync.stream());
+    .pipe(rename({ suffix: ".min" }))
+    .pipe(replace(/\/\*#\s*sourceMappingURL=.*\*\//g, ""))
+    .pipe(dest(paths.cssDest));
 }
 
-// JS: Minify & Bundle
+// -------------------- SCRIPTS --------------------
+
 function scripts() {
-  return gulp.src(paths.js)
+  return src(paths.js)
     .pipe(sourcemaps.init())
-    .pipe(concat('main.js'))
+    .pipe(concat("main.js"))
     .pipe(babel({ presets: ["@babel/env"] }))
     .pipe(terser())
-    .pipe(rename({ suffix: '.min' }))
-    .pipe(sourcemaps.write('.'))
-    .pipe(gulp.dest(paths.jsDest))
-    .pipe(browserSync.stream());
+    .pipe(rename({ suffix: ".min" }))
+    .pipe(sourcemaps.write("."))
+    .pipe(dest(paths.jsDest))
+    .pipe(isNetlify ? dest(paths.jsDest) : browserSync.stream());
 }
 
-// HTML includes
+// -------------------- HTML --------------------
+
 function html() {
-  return gulp.src(paths.html)
+  return src(paths.html)
     .pipe(fileInclude({ prefix: "@@", basepath: "@file" }))
-    .pipe(gulp.dest(paths.htmlDest))
-    .pipe(browserSync.stream());
+    .pipe(dest(paths.htmlDest))
+    .pipe(isNetlify ? dest(paths.htmlDest) : browserSync.stream());
 }
 
-// Watch files + Live reload
+// -------------------- WATCH (DEV ONLY) --------------------
+
 function watchFiles() {
   browserSync.init({
     server: { baseDir: "dist", index: "index.html" },
     port: 8080,
   });
 
-  gulp.watch("src/scss/**/*.scss", gulp.series(stylesDev, stylesProd));
-  gulp.watch("src/js/**/*.js", scripts);
-  gulp.watch("src/html/**/*.html", html);
+  watch("src/scss/**/*.scss", stylesDev);
+  watch("src/js/**/*.js", scripts);
+  watch("src/html/**/*.html", html);
 }
 
-// Gulp tasks
-gulp.task("stylesDev", stylesDev);
-gulp.task("stylesProd", stylesProd);
-gulp.task("scripts", scripts);
-gulp.task("html", html);
-gulp.task("watch", gulp.series("html", "scripts", stylesDev, stylesProd, watchFiles));
-gulp.task("build", gulp.series(stylesDev, stylesProd, scripts, html));
-gulp.task("default", gulp.series("build", watchFiles));
+// -------------------- TASK EXPORTS --------------------
+
+// 🔥 Netlify build task
+exports.build = series(stylesProd, scripts, html);
+
+// 👨‍💻 Local development
+exports.dev = series(html, scripts, stylesDev, watchFiles);
+
+// Default = dev
+exports.default = exports.dev;
